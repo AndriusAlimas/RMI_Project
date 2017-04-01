2952b16685eefde8df993d29ff6e48dbbc62c44e
Andrius <andriusjavait@gmail.com>
2017-04-02 00:31:13 +0100

    Create a RMI_Client we use as Servlet called ClientServlet

--- RMI_Project/src/main/java/com/andrius/project/RMI_Project/servlets/ClientServlet.java
+++ RMI_Project/src/main/java/com/andrius/project/RMI_Project/servlets/ClientServlet.java
@@ -1,10 +1,15 @@
 package com.andrius.project.RMI_Project.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.rmi.Naming;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.andrius.project.RMI_Project.interfaces.AddI;
 
 public class ClientServlet extends HttpServlet {
 
@@ -13,8 +18,20 @@ public class ClientServlet extends HttpServlet {
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		
+		int sum = 0;
+		// As a client you dont know where is AddC object
+		try{
+		AddI obj = (AddI) Naming.lookup("ADD");
+		 sum = obj.add(5,4);
+		}catch(Exception exc){
+			exc.printStackTrace();
+		}
+		
+
+		pw.println("<h1>We get answer remote : " + sum + "</h1>");
 	}
 
 }
