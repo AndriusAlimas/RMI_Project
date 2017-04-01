63a43a94882eac248d11bf3823c8180730d9a01c
Andrius <andriusjavait@gmail.com>
2017-04-02 00:51:10 +0100

    added default serialization number for AddC.java

--- RMI_Project/src/main/java/com/andrius/project/RMI_Project/others/AddC.java
+++ RMI_Project/src/main/java/com/andrius/project/RMI_Project/others/AddC.java
@@ -8,6 +8,11 @@ import com.andrius.project.RMI_Project.interfaces.AddI;
 // obtaining a stub that communicates to the remote object we need extend UnicastRemoteObject
 public class AddC extends UnicastRemoteObject implements AddI{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	// if you extend UnicastRemoteObject, you must have provide default constructor:
 	public AddC() throws RemoteException {
 		super();
